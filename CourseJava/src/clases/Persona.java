d3b8b0d4bc1695fa26f78e18b2422e16fc3f6f2d
David Antonio Olmos Ruiz <jamesdavid9810@outlook.com>
2023-09-02 00:07:07 -0600

    Ejemplos de Clases
    Agregamos un pequeño ejemplo de clases con creaion de objetos y concatenacion

---
+++ CourseJava/src/clases/Persona.java
@@ -0,0 +1,15 @@
+
+package clases;
+
+
+public class Persona {
+    
+     //Definimos los atributos de la clase
+    String nombre;
+    String apellido;
+    
+    public void despelgarInformacion(){
+        System.out.println("Nombre:" + nombre);
+        System.out.println("Apellido:" + apellido);
+    }
+}
